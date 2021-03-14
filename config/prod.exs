b7398179b51ceb425e7f985ff4c1a566ea74247f
jomaro <jomaro.rodrigues@gmail.com>
2021-03-14 00:28:53 -0300

    try deploing to gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,27 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dallas, DallasWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "dallas.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :dallas, DallasWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  secret_key_base: secret_key_base
+
+config :dallas, DallasWeb.Endpoint, server: true
 
 # ## SSL Support
 #
@@ -52,4 +68,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,17 +4,17 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+# database_url =
+#   System.get_env("DATABASE_URL") ||
+#     raise """
+#     environment variable DATABASE_URL is missing.
+#     For example: ecto://USER:PASS@HOST/DATABASE
+#     """
 
-config :dallas, Dallas.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+# config :dallas, Dallas.Repo,
+#   # ssl: true,
+#   url: database_url,
+#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -40,6 +40,8 @@ config :dallas, DallasWeb.Endpoint,
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
 
-config :dallas, whitelisted_instruments: [
-  Dallas.Instrument.TzData,
-]
+config :dallas, DallasWeb.Endpoint, server: true
+
+# config :dallas, whitelisted_instruments: [
+#   Dallas.Instrument.TzData.America,
+# ]
