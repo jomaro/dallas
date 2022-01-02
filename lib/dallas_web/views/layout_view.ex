085f1553bde6093df2992c88f8c0b3da77dd0c3c
jomaro <jomaro.rodrigues@gmail.com>
2022-01-02 14:12:37 -0300

    add favicon

--- lib/dallas_web/views/layout_view.ex
+++ lib/dallas_web/views/layout_view.ex
@@ -1,3 +1,16 @@
 defmodule DallasWeb.LayoutView do
   use DallasWeb, :view
+
+  @favicon_non_dashboard "❤️"
+  @favicon_ok "👍"
+  @favicon_error "👎"
+
+  def favicon(assigns) do
+    case Map.get(assigns, :current_node) do
+      nil -> @favicon_non_dashboard
+      %{level: :ok} -> @favicon_ok
+      %{level: :none} -> @favicon_ok
+      _ -> @favicon_error
+    end
+  end
 end
